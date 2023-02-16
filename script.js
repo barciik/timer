3dd01543b0e18eb8cb47a37e26166d710241122f
bart <92023758+barciik@users.noreply.github.com>
2023-02-16 18:20:36 +0100

    layout improvements

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const timer = document.getElementById('timer');
+const dayCount = document.getElementById('days');
+const hourCount = document.getElementById('hours');
+const minuteCount = document.getElementById('minutes');
+const secondCount = document.getElementById('seconds');
 const destination = new Date('December 31, 2023 20:00:00');
 // const date = Math.abs(destination - new Date());
 // const hours = Math.floor(date / 1000 / 60 / 60);
@@ -7,15 +10,19 @@ const destination = new Date('December 31, 2023 20:00:00');
 
 setInterval(() => {
 	const date = Math.abs(destination - new Date());
-	const hours = Math.floor(date / 1000 / 60 / 60);
-	const minutes = Math.floor((date / 1000 / 60 / 60 - hours) * 60);
-	const seconds = Math.floor(((date / 1000 / 60 / 60 - hours) * 60 - minutes) * 60);
+	const days = Math.floor(date / 1000 / 60 / 60 / 24);
+	const hours = Math.floor((date / 1000 / 60 / 60 / 24 - days) * 24);
+	const minutes = Math.floor((((date / 1000 / 60 / 60 / 24 - days) * 24) - hours) * 60);
+	const seconds = Math.floor(((((date / 1000 / 60 / 60 / 24 - days) * 24) - hours) * 60 - minutes) * 60);
+	
 
-    timer.innerText = `${hours}:${minutes}:${seconds}s`
+	dayCount.innerText = `${days} dni`;
+	hourCount.innerText = `${hours} godzin`;
+	minuteCount.innerText = `${minutes} minut`;
+	secondCount.innerText = `${seconds} sekund`;
 }, 100);
 
 // console.log(timer);
 // console.log(hours);
 // console.log(minutes);
 // console.log(seconds);
-
